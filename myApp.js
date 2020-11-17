fc70e64b0dd4966f267640d625fc4744862682ee
Glitch (simplistic-almanac) <none>
2020-11-17 11:11:13 +0000

    Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -9,11 +9,11 @@
 /** 1) Install & Set up mongoose */
 
 // Add `mongodb` and `mongoose` to the project's `package.json`. Then require 
-// `mongoose`. Store your **mLab** database URI in the private `.env` file 
+// `mongoose`. Store your **MongoDB** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
